c1c8d53e41762431f9dd9f751393b0dd35c3c512
Akash Mahadik <akashpm1996@gmail.com>
2025-05-29 00:09:59 +0530

    Setup for Github Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-25',
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/Madone-SLR-7-Gen-7/' : '/',
+  },
   ssr: false,
   css: ['/node_modules/locomotive-scroll/dist/locomotive-scroll.css'],
   modules: ["@nuxtjs/tailwindcss"]
